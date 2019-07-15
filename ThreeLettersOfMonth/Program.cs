ce27e8d51de21d82778a4f2d2a5da14f8e08ef50
beata.krasnopolska@gmail.com <beata.krasnopolska@gmail.com>
2019-07-15 21:53:13 +0200

    Add project files.

---
+++ ThreeLettersOfMonth/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ThreeLettersOfMonth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program displays the name of the first three letters of month of a year starting form current date");
+            var today = DateTime.Today;
+
+            DateTime now = DateTime.Now;
+            Console.WriteLine("Today is: {0}", today);
+            Console.WriteLine("The tweleve months are:");
+            for(int i =0; i< 12; i++)
+            {
+                Console.WriteLine("{0}", now.ToString("MMM"));
+                now = now.AddMonths(1);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
